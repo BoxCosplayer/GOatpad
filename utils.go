64215d1bbdcb65bd50ad74afb642c6c0dcd44f3f
BoxCosplayer <rajveer@sandhuhome.uk>
2026-01-11 02:23:40 +0800

    fix(copypaste-symbols): Fixed detection for searching to the left

--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@ package main
 
 import "unicode"
 
-func isStringAlphaNumeric(s string) bool {
+func is_string_alphanumeric(s string) bool {
 	for _, r := range s {
 		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
 			return false
@@ -18,22 +18,47 @@ func get_symbol_from_line(line []rune, startingIndex int) (int, int) {
 
 	// While True Equivalent
 	currentCharacter := string(line[startingIndex])
-	if isStringAlphaNumeric(currentCharacter) {
-		for right := 1; right == 0; right++ {
-			currentCharacter += string(line[startingIndex+right])
-			if isStringAlphaNumeric(currentCharacter) == false {
-				rightIndex += right - 1
-				right = 0
-			}
-		}
+	if is_string_alphanumeric(currentCharacter) {
 		for left := 1; left == 0; left++ {
-			currentCharacter += string(line[startingIndex+left])
-			if isStringAlphaNumeric(currentCharacter) == false {
+			currentCharacter += string(line[startingIndex-left])
+			if is_string_alphanumeric(currentCharacter) == false {
 				leftIndex -= left - 1
 				left = 0
+			}
+		}
+		for right := 1; right == 0; right++ {
+			currentCharacter += string(line[startingIndex+right])
+			if is_string_alphanumeric(currentCharacter) == false {
+				rightIndex += right - 1
+				right = 0
 			}
 		}
 	}
 
 	return leftIndex, rightIndex
+}
+
+func does_line_contain_rune(line []rune, searchRune rune) bool {
+	for _, r := range line {
+		if r == searchRune {
+			return true
+		}
+	}
+	return false
+}
+
+func find_current_block(counter int) (int, int) {
+
+	leftLineIndex, rightLineIndex := currentRow, currentRow
+
+	for ; counter != 0; counter-- {
+
+		for ; does_line_contain_rune(textBuffer[leftLineIndex], '{'); leftLineIndex-- {
+			if leftLineIndex <= 1 {
+				return 0, 0
+			}
+		}
+	}
+
+	return leftLineIndex, rightLineIndex
 }
